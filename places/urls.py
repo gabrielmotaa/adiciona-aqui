d2a4f9f287827e8eee738db338eb7537efa09315
Gabriel da Mota <gabriel.mota@dasa.com.br>
2023-04-11 23:23:33 -0300

    Correção de consistência de url

--- places/urls.py
+++ places/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('lugares/', views.PlaceListView.as_view(), name='place_list'),
-    path('lugares/<int:pk>', views.PlaceDetailView.as_view(), name='place_detail'),
+    path('lugares/<int:pk>/', views.PlaceDetailView.as_view(), name='place_detail'),
     path('lugares/<int:pk>/atualizar/', views.PlaceUpdateView.as_view(), name='place_update'),
     path('lugares/adicionar/', views.PlaceCreateView.as_view(), name='place_create'),
 ]
