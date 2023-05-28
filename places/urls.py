421742fdf909085613ae87d67077e688d90dd921
Gabriel da Mota <gabriel.mota@dasa.com.br>
2023-05-28 00:25:42 -0300

    Typo

--- places/urls.py
+++ places/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('locais/', views.PlaceListView.as_view(), name='place_list'),
     path('locais/<int:pk>/', views.PlaceDetailView.as_view(), name='place_detail'),
-    path('lcaiss/<int:pk>/atualizar/', views.PlaceUpdateView.as_view(), name='place_update'),
+    path('locais/<int:pk>/atualizar/', views.PlaceUpdateView.as_view(), name='place_update'),
     path('locais/adicionar/', views.PlaceCreateView.as_view(), name='place_create'),
 ]
