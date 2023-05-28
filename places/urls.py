e8b068647b1184c190d2b2a14aa5080cbae26f8f
Gabriel da Mota <gabriel.mota@dasa.com.br>
2023-05-28 00:15:32 -0300

    Pequenas correções de nomenclatura

--- places/urls.py
+++ places/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('lugares/', views.PlaceListView.as_view(), name='place_list'),
-    path('lugares/<int:pk>/', views.PlaceDetailView.as_view(), name='place_detail'),
-    path('lugares/<int:pk>/atualizar/', views.PlaceUpdateView.as_view(), name='place_update'),
-    path('lugares/adicionar/', views.PlaceCreateView.as_view(), name='place_create'),
+    path('locais/', views.PlaceListView.as_view(), name='place_list'),
+    path('locais/<int:pk>/', views.PlaceDetailView.as_view(), name='place_detail'),
+    path('lcaiss/<int:pk>/atualizar/', views.PlaceUpdateView.as_view(), name='place_update'),
+    path('locais/adicionar/', views.PlaceCreateView.as_view(), name='place_create'),
 ]
