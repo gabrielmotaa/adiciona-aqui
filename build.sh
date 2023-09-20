3f141f1ae9b3c6c76f22fc98b70bdaf9040e5d56
Gabriel da Mota <gabriel.mota@dasa.com.br>
2023-09-19 23:49:41 -0300

    Mudanças para deploy em fly.io

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
+#!/bin/sh
 set -o errexit
 
-pip install pip --upgrade
-pip install -r requirements/prod.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py shell -c "from django.contrib.auth import get_user_model;User = get_user_model(); User.objects.filter(is_superuser=True).delete()"
-python manage.py createsuperuser --noinput
-python manage.py populate_categories
+./manage.py collectstatic --no-input
+./manage.py migrate
+./manage.py shell -c "from django.contrib.auth import get_user_model;get_user_model().objects.filter(is_superuser=True).delete()"
+./manage.py createsuperuser --noinput
+./manage.py populate_categories
