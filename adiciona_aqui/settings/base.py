8e96db1294f64b2fa989f7c6efe58a2fd4e55bf3
Gabriel da Mota <gabrielmotaa@hotmail.com>
2023-11-15 20:13:41 -0300

    Tags de i18n movidas para builtins

--- adiciona_aqui/settings/base.py
+++ adiciona_aqui/settings/base.py
@@ -72,6 +72,7 @@ TEMPLATES = [
         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
+            'builtins': ['django.templatetags.i18n'],
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
