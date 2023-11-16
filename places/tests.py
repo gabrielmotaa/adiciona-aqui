9995d98be9625299bc2edb99624f237b2b28f74a
Gabriel da Mota <gabrielmotaa@hotmail.com>
2023-11-16 01:59:35 -0300

    Adicionado testes

--- places/tests.py
+++ places/tests.py
@@ -1,3 +1,272 @@
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.shortcuts import reverse
 from django.test import TestCase
+from django.utils import translation
 
-# Create your tests here.
+from places.forms import PlaceForm
+from places.models import Category, Place
+
+
+class PlacesViewsTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = get_user_model().objects.create_user(
+            email="test@email.com", password="foo"
+        )
+
+        for i in range(3):
+            category = Category.objects.create(name=f"Category {i}")
+
+            place = Place.objects.create(
+                name=f"Place {i+1}",
+                address=f"Address {i+1}",
+                user=user,
+            )
+
+            place.categories.add(category)
+
+        cls.user = user
+
+    def setUp(self):
+        self.client.login(email="test@email.com", password="foo")
+
+    def test_place_list_view(self):
+        response = self.client.get(reverse("place_list"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_place_list_view_correct_template(self):
+        response = self.client.get(reverse("place_list"))
+        self.assertTemplateUsed(response, "places/place_list.html")
+
+    def test_place_list_view_only_authenticated_users(self):
+        self.client.logout()
+        response = self.client.get(reverse("place_list"))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, reverse("login") + "?next=" + reverse("place_list")
+        )
+
+    def test_place_list_view_correct_url_english(self):
+        response = self.client.get("/en/places/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>My Places</title>")
+
+    def test_place_list_view_correct_url_portuguese(self):
+        response = self.client.get("/locais/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Meus locais</title>")
+
+    def test_place_detail_view(self):
+        response = self.client.get(reverse("place_detail", kwargs={"pk": "1"}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_place_detail_view_correct_template(self):
+        response = self.client.get(reverse("place_detail", kwargs={"pk": "1"}))
+        self.assertTemplateUsed(response, "places/place_detail.html")
+
+    def test_place_detail_view_only_authenticated_users(self):
+        self.client.logout()
+        response = self.client.get(reverse("place_detail", kwargs={"pk": "1"}))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response,
+            reverse("login") + "?next=" + reverse("place_detail", kwargs={"pk": "1"}),
+        )
+
+    def test_place_detail_view_correct_url_english(self):
+        response = self.client.get("/en/places/1/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Place - Place 1</title>")
+
+    def test_place_detail_view_correct_url_portuguese(self):
+        response = self.client.get("/locais/1/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Local - Place 1</title>")
+
+    def test_place_create_view(self):
+        response = self.client.get(reverse("place_create"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_place_create_view_add_new_place(self):
+        data = {"name": "New Place", "address": "New Street", "categories": ["1"]}
+
+        response = self.client.post(reverse("place_create"), data=data)
+        self.assertRedirects(response, reverse("place_detail", kwargs={"pk": "4"}))
+
+        # Usuário deve estar relacionado à local
+        self.assertEqual(Place.objects.get(pk=4).user, self.user)
+
+    def test_place_create_view_correct_template(self):
+        response = self.client.get(reverse("place_create"))
+        self.assertTemplateUsed(response, "places/place_create.html")
+
+    def test_place_create_view_only_authenticated_users(self):
+        self.client.logout()
+        response = self.client.get(reverse("place_create"))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, reverse("login") + "?next=" + reverse("place_create")
+        )
+
+    def test_place_create_view_correct_url_english(self):
+        response = self.client.get("/en/places/add/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Add a new place</title>")
+
+    def test_place_create_view_correct_url_portuguese(self):
+        response = self.client.get("/locais/adicionar/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Adicionar local</title>")
+
+    def test_place_update_view(self):
+        response = self.client.get(reverse("place_update", kwargs={"pk": "1"}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_place_update_view_update_existing_place(self):
+        category = Category.objects.create(name="Some category")
+        place = Place.objects.create(
+            name="Some Place",
+            address="Some Address",
+            user=self.user,
+        )
+        place.categories.add(category)
+
+        data = {
+            "name": "Some Place Updated",
+            "address": "Some Address",
+            "categories": [str(category.pk)],
+        }
+
+        response = self.client.post(
+            reverse("place_update", kwargs={"pk": "1"}), data=data, follow=True
+        )
+        self.assertContains(response, "Some Place Updated")
+
+    def test_place_update_view_accessing_other_user_place(self):
+        self.client.logout()
+        get_user_model().objects.create_user(email="other@email.com", password="foo")
+        self.client.login(email="other@email.com", password="foo")
+
+        response = self.client.get(reverse("place_update", kwargs={"pk": "1"}))
+        self.assertEqual(response.status_code, 403)
+
+    def test_place_update_view_correct_template(self):
+        response = self.client.get(reverse("place_update", kwargs={"pk": "1"}))
+        self.assertTemplateUsed(response, "places/place_update.html")
+
+    def test_place_update_view_only_authenticated_users(self):
+        self.client.logout()
+        response = self.client.get(reverse("place_update", kwargs={"pk": "1"}))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response,
+            reverse("login") + "?next=" + reverse("place_update", kwargs={"pk": "1"}),
+        )
+
+    def test_place_update_view_correct_url_english(self):
+        response = self.client.get("/en/places/1/update/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Update - Place 1</title>")
+
+    def test_place_update_view_correct_url_portuguese(self):
+        response = self.client.get("/locais/1/atualizar/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Atualizar - Place 1</title>")
+
+
+class PlacesModelsTestCase(TestCase):
+    def setUp(self):
+        with translation.override("en"):
+            self.user = get_user_model().objects.create_user(
+                email="test@email.com", password="foo"
+            )
+            self.category = Category.objects.create(name="Test Category")
+            self.place = Place.objects.create(
+                name="Test Place",
+                address="Test Address",
+                phone="123456789",
+                site="http://www.example.com",
+                registered=True,
+                user=self.user,
+                image="images/test_image.jpg",
+            )
+            self.place.categories.add(self.category)
+
+    def test_category_str(self):
+        self.assertEqual(str(self.category), "Test Category")
+
+    def test_place_str(self):
+        self.assertEqual(str(self.place), "Test Place")
+
+    def test_place_get_absolute_url(self):
+        url = reverse("place_detail", kwargs={"pk": self.place.pk})
+        self.assertEqual(self.place.get_absolute_url(), url)
+
+    def test_place_has_category(self):
+        self.assertEqual(self.place.categories.first(), self.category)
+
+    def test_place_created_at_auto_now_add(self):
+        self.assertIsNotNone(self.place.created_at)
+
+    def test_place_image_field_upload_to(self):
+        self.assertEqual(self.place.image.field.upload_to, "images")
+
+    def test_place_registered_default_value(self):
+        self.assertTrue(self.place.registered)
+
+    def test_place_user_foreign_key(self):
+        self.assertEqual(self.place.user, self.user)
+
+
+class PlaceFormTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        for i in range(2):
+            Category.objects.create(name=f"Category {i}")
+
+    def test_valid_data(self):
+        form_data = {
+            "name": "Test Place",
+            "address": "Test Address",
+            "phone": "123456789",
+            "site": "http://www.example.com",
+            "categories": [1, 2],
+        }
+        form = PlaceForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_missing_required_data(self):
+        form_data = {
+            "name": "",
+            "address": "Test Address",
+            "phone": "123456789",
+            "site": "http://www.example.com",
+            "categories": [1, 2],
+        }
+        form = PlaceForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("name", form.errors)
+
+    def test_invalid_image_extension(self):
+        form_data = {
+            "name": "Test Place",
+            "address": "Test Address",
+            "phone": "123456789",
+            "site": "http://www.example.com",
+            "categories": [1, 2],
+        }
+        form = PlaceForm(
+            data=form_data,
+            files={
+                "image": SimpleUploadedFile(
+                    name="file.txt", content=b"", content_type="text/plain"
+                )
+            },
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn("image", form.errors)
--- users/tests.py
+++ users/tests.py
@@ -1,9 +1,16 @@
-# Referência: https://testdriven.io/blog/django-custom-user-model/
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.contrib.auth.tokens import default_token_generator
+from django.core import mail
+from django.db import IntegrityError
+from django.shortcuts import reverse
+from django.test import RequestFactory, TestCase
+from django.utils.http import urlsafe_base64_encode
+
+from users.forms import CustomUserChangeForm, CustomUserCreationForm
 
 
 class UsersManagersTests(TestCase):
+    # Referência: https://testdriven.io/blog/django-custom-user-model/
 
     def test_create_user(self):
         User = get_user_model()
@@ -27,7 +34,9 @@ class UsersManagersTests(TestCase):
 
     def test_create_superuser(self):
         User = get_user_model()
-        admin_user = User.objects.create_superuser(email="super@user.com", password="foo")
+        admin_user = User.objects.create_superuser(
+            email="super@user.com", password="foo"
+        )
         self.assertEqual(admin_user.email, "super@user.com")
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
@@ -40,4 +49,208 @@ class UsersManagersTests(TestCase):
             pass
         with self.assertRaises(ValueError):
             User.objects.create_superuser(
-                email="super@user.com", password="foo", is_superuser=False)
+                email="super@user.com", password="foo", is_superuser=False
+            )
+
+
+class UsersViewsTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_signup_view(self):
+        response = self.client.post(
+            reverse("signup"),
+            data={
+                "email": "test@email.com",
+                "password1": "mysuperduperpassword",
+                "password2": "mysuperduperpassword",
+            },
+        )
+        self.assertEqual(get_user_model().objects.count(), 1)
+        self.assertRedirects(response, reverse("login"))
+
+    def test_signup_view_correct_template(self):
+        response = self.client.get(reverse("signup"))
+        self.assertTemplateUsed(response, "users/signup.html")
+
+    def test_signup_view_correct_url_english(self):
+        response = self.client.get("/en/users/signup/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Signup</title>")
+
+    def test_signup_view_correct_url_portuguese(self):
+        response = self.client.get("/usuarios/registrar/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Registrar-se</title>")
+
+    def test_login_view(self):
+        email = "test@email.com"
+        password = "foo"
+
+        get_user_model().objects.create_user(email=email, password=password)
+        # No login o campo do e-mail é username, diferentemente do registro
+        response = self.client.post(
+            reverse("login"),
+            data={
+                "username": email,
+                "password": password,
+            },
+        )
+
+        self.assertRedirects(response, reverse("home"))
+
+    def test_login_view_correct_template(self):
+        response = self.client.get(reverse("login"))
+        self.assertTemplateUsed(response, "users/login.html")
+
+    def test_login_view_correct_url_english(self):
+        response = self.client.get("/en/users/login/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Log In</title>")
+
+    def test_login_view_correct_url_portuguese(self):
+        response = self.client.get("/usuarios/entrar/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Log In</title>")
+
+    def test_logout_view(self):
+        email = "test@email.com"
+        password = "foo"
+
+        get_user_model().objects.create_user(email=email, password=password)
+
+        self.client.login(email=email, password=password)
+        response = self.client.get(reverse("logout"))
+
+        self.assertRedirects(response, reverse("home"))
+
+    def test_password_reset_view(self):
+        get_user_model().objects.create_user(email="test@email.com", password="foo")
+        response = self.client.post(
+            reverse("password_reset"),
+            data={
+                "email": "test@email.com",
+            },
+        )
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertRedirects(response, reverse("password_reset_done"))
+
+    def test_password_reset_view_correct_template(self):
+        response = self.client.get(reverse("password_reset"))
+        self.assertTemplateUsed(response, "users/password_reset.html")
+
+    def test_password_reset_view_correct_url_english(self):
+        response = self.client.get("/en/users/password-reset/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Forgot your password?</title>")
+
+    def test_password_reset_view_correct_url_portuguese(self):
+        response = self.client.get("/usuarios/redefinir-senha/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Esqueceu sua senha?</title>")
+
+    def test_password_reset_done_view_correct_template(self):
+        response = self.client.get(reverse("password_reset_done"))
+        self.assertTemplateUsed(response, "users/password_reset_done.html")
+
+    def test_password_reset_confirm_view_correct_template(self):
+        user = get_user_model().objects.create_user(
+            email="test@email.com", password="foo"
+        )
+        token = default_token_generator.make_token(user)
+        uidb64 = urlsafe_base64_encode(bytes(user.pk))
+
+        response = self.client.get(
+            reverse("password_reset_confirm", kwargs={"uidb64": uidb64, "token": token})
+        )
+        self.assertTemplateUsed(response, "users/password_reset_confirm.html")
+
+    def test_password_reset_confirm_view_correct_url_portuguese(self):
+        user = get_user_model().objects.create_user(
+            email="test@email.com", password="foo"
+        )
+        token = default_token_generator.make_token(user)
+        uidb64 = urlsafe_base64_encode(bytes(user.pk))
+
+        response = self.client.get(f"/usuarios/redefinir/{uidb64}/{token}/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Insira sua nova senha</title>")
+
+    def test_password_reset_confirm_view_correct_url_english(self):
+        user = get_user_model().objects.create_user(
+            email="test@email.com", password="foo"
+        )
+        token = default_token_generator.make_token(user)
+        uidb64 = urlsafe_base64_encode(bytes(user.pk))
+
+        response = self.client.get(f"/en/users/reset/{uidb64}/{token}/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Enter your new password</title>")
+
+    def test_password_reset_complete_view_correct_template(self):
+        response = self.client.get(reverse("password_reset_complete"))
+        self.assertTemplateUsed(response, "users/password_reset_complete.html")
+
+    def test_password_reset_complete_view_correct_url_english(self):
+        response = self.client.get("/en/users/reset/done/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Password reset completed</title>")
+
+    def test_password_reset_complete_view_correct_url_portuguese(self):
+        response = self.client.get("/usuarios/redefinir/pronto/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<title>Redefinição de senha concluída</title>")
+
+
+class CustomUserFormsTestCase(TestCase):
+    def setUp(self):
+        self.user_data = {
+            "email": "test@email.com",
+            "password1": "footestpassword",
+            "password2": "footestpassword",
+        }
+
+    def test_custom_user_creation_form_valid(self):
+        form = CustomUserCreationForm(data=self.user_data)
+        self.assertTrue(form.is_valid())
+
+    def test_custom_user_creation_form_invalid(self):
+        invalid_data = self.user_data.copy()
+        invalid_data["email"] = ""
+        form = CustomUserCreationForm(data=invalid_data)
+        self.assertFalse(form.is_valid())
+
+    def test_custom_user_change_form_valid(self):
+        user = get_user_model().objects.create_user(
+            email="test@email.com", password="foo"
+        )
+        form_data = {"email": "newemail@example.com"}
+        form = CustomUserChangeForm(data=form_data, instance=user)
+        self.assertTrue(form.is_valid())
+
+    def test_custom_user_change_form_invalid(self):
+        user = get_user_model().objects.create_user(
+            email="test@email.com", password="foo"
+        )
+        invalid_form_data = {"email": ""}
+        form = CustomUserChangeForm(data=invalid_form_data, instance=user)
+        self.assertFalse(form.is_valid())
+
+
+class CustomUserTests(TestCase):
+    def test_email_field_unique(self):
+        email = "test@email.com"
+        password = "foo"
+
+        get_user_model().objects.create_user(email=email, password=password)
+        with self.assertRaises(IntegrityError):
+            get_user_model().objects.create_user(email=email, password=password)
+
+    def test_email_str(self):
+        email = "test@email.com"
+        password = "foo"
+
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(str(user), email)
